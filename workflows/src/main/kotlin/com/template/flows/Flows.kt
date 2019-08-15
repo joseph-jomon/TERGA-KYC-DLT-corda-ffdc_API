ce93f14d9956f3e5c9a448da10a8dea7098b4eec
joseph-jomon <54072683+joseph-jomon@users.noreply.github.com>
2019-08-15 19:47:41 +0530

    kyc-flow-code-implemented

--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -1,23 +1,16 @@
 package com.template.flows
-import  com.template.states.KycState
 
 import co.paralleluniverse.fibers.Suspendable
 import com.template.contracts.KycContract
-import net.corda.core.flows.*
-import net.corda.core.utilities.ProgressTracker
+import com.template.states.KycState
 import net.corda.core.contracts.Command
-import net.corda.core.identity.Party
-import net.corda.core.transactions.TransactionBuilder
-
-import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.ContractState
-import net.corda.core.identity.AbstractParty
-
-
-
-
-
+import net.corda.core.contracts.requireThat
+import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
 
 
 // *********
@@ -36,18 +29,26 @@ class KycFlow(val kname: String = "not_shared",val kaddress: String = "not_share
         val notary = serviceHub.networkMapCache.notaryIdentities[0]
         // We create the transaction components.
         val outputState = KycState(kname,kaddress,kdob,kemail, ourIdentity, otherParty)
-        val command = Command(Type, ourIdentity.owningKey)
+        val command = Command(Type, listOf(ourIdentity.owningKey, otherParty.owningKey))
 
         // We create a transaction builder and add the components.
         val txBuilder = TransactionBuilder(notary = notary)
                 .addOutputState(outputState, KycContract.ID)
                 .addCommand(command)
+        // Verifying the transaction.
+        txBuilder.verify(serviceHub)
+
         // We sign the transaction.
         val signedTx = serviceHub.signInitialTransaction(txBuilder)
+
         // Creating a session with the other party.
         val otherPartySession = initiateFlow(otherParty)
+
+        // Obtaining the counterparty's signature.
+        val fullySignedTx = subFlow(CollectSignaturesFlow(signedTx, listOf(otherPartySession), CollectSignaturesFlow.tracker()))
+
         // We finalise the transaction and then send it to the counterparty.
-        subFlow(FinalityFlow(signedTx, otherPartySession))
+        subFlow(FinalityFlow(fullySignedTx, otherPartySession))
 
 
         // Initiator flow logic goes here.
@@ -59,6 +60,15 @@ class KycFlowResponder(  val otherPartySession: FlowSession) : FlowLogic<Unit>()
     @Suspendable
     override fun call() {
         // Responder flow logic goes here.
+        val signTransactionFlow = object : SignTransactionFlow(otherPartySession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                val output = stx.tx.outputs.single().data
+                "This must be an IOU transaction." using (output is KycState)
+
+            }
+        }
+
+        val expectedTxId = subFlow(signTransactionFlow).id
         subFlow(ReceiveFinalityFlow(otherPartySession))
 
     }
